829b3d7454798dff71f7502adf4f6dd254899278
mattrichard <matthew.w.richard@gmail.com>
2015-04-09 11:16:37 -0600

    Commented out FFT function so the project will compile

--- mainwindow.h
+++ mainwindow.h
@@ -8,7 +8,7 @@ class MainWindow : public QObject
     Q_OBJECT;
 
     public slots:
-        bool Menu_Frequency_FFT(Image &image);
+        //bool Menu_Frequency_FFT(Image &image);
         bool Menu_Frequency_fftw_fft(Image &image);
         bool Menu_Histogram_Stretch(Image &image);
 };

e1fdb3ba449c47bc85c2dfe8e46f8e0172405c88
mattrichard <matthew.w.richard@gmail.com>
2015-04-11 14:04:00 -0600

    Added the ideal low pass filter.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,7 @@ Date:   Feb 2015
 #include "mainwindow.h"
 
 #include <cmath>
+#include <iostream>
 
 /******************************************************************************
  * Function: Menu_Palette_Grayscale
@@ -103,6 +104,7 @@ bool MainWindow::Menu_Frequency_fftw_fft_v2(Image &image)
 
     in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nrows * ncols);
 
+    // Initialize in variable using image
     for(int i = 0; i < nrows; i++)
     {
         for(int j = 0; j < ncols; j++)
@@ -119,11 +121,11 @@ bool MainWindow::Menu_Frequency_fftw_fft_v2(Image &image)
     grayscale(image);
 
     double max_mag = 0;
+    // Calculate magnitudes and find max magnitude
     for(int i = 0; i < nrows; i++)
     {
         for(int j = 0; j < ncols; j++)
         {
-            //mag = sqrt(out[i*ncols + j][0] * out[i*ncols + j][0]
             in[i*ncols + j][0] = sqrt(out[i*ncols + j][0] * out[i*ncols + j][0]
                 + out[i*ncols + j][1] * out[i*ncols + j][1]);
 
@@ -161,4 +163,94 @@ void MainWindow::fft(fftw_complex* in, fftw_complex* out, int nrows, int ncols, 
     fftw_execute(plan);
 
     fftw_destroy_plan(plan);
+}
+
+bool MainWindow::Menu_Frequency_Ideal_Low_Pass(Image &image)
+{
+    fftw_complex* in;
+    fftw_complex* out;
+    fftw_complex* out2;
+
+    if(image.IsNull())
+        return false;
+
+    double r = 10;
+    if(!Dialog("Low Pass Filter").Add(r, "radius").Show())
+        return false;
+
+    if(r <= 0)
+        return false;
+
+    int nrows = image.Height();
+    int ncols = image.Width();
+
+    in = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nrows * ncols);\
+    out2 = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nrows * ncols);
+
+    for(int i = 0; i < nrows; i++)
+    {
+        for(int j = 0; j < ncols; j++)
+        {
+            in[i*ncols + j][0] = image[i][j] * ((i + j) % 2 == 0 ? 1 : -1);
+            in[i*ncols + j][1] = 0;
+        }
+    }
+
+    out = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * nrows * ncols);
+
+    fft(in, out, nrows, ncols, FFTW_FORWARD);
+
+    int center_x = ncols / 2.0;
+    int center_y = nrows / 2.0;
+
+    // low pass filter
+    for(int i = 0; i < nrows; i++)
+    {
+        for(int j = 0; j < ncols; j++)
+        {
+            if(r < sqrt((i - center_y) * (i - center_y) + (j - center_x) * (j - center_x)))
+            {
+                out[i*ncols + j][0] = 0;
+                out[i*ncols + j][1] = 0;
+            }
+        }
+    }
+
+    fft(out, out2, nrows, ncols, FFTW_BACKWARD);
+
+    double max_mag = 0;
+    // compute magnitude and find max magnitude
+    for(int i = 0; i < nrows; i++)
+    {
+        for(int j = 0; j < ncols; j++)
+        {
+            in[i*ncols + j][0] = sqrt(out2[i*ncols + j][0] * out2[i*ncols + j][0]
+                + out2[i*ncols + j][1] * out2[i*ncols + j][1]);
+
+            if(max_mag < in[i*ncols + j][0])
+                max_mag = in[i*ncols + j][0];
+        }
+    }
+
+    double mag;
+    // scale image
+    for(int i = 0; i < nrows; i++)
+    {
+        for(int j = 0; j < ncols; j++)
+        {
+            mag = (int)(255.0 / max_mag * in[i*ncols + j][0]);
+            if(mag < 0)
+                mag = 0;
+            else if(mag > 255)
+                mag = 255;
+
+            image[i][j] = mag;
+        }
+    }
+
+    fftw_free(in);
+    fftw_free(out2);
+    fftw_free(out);
+
+    return true;
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,9 +15,10 @@ class MainWindow : public QObject
         //bool Menu_Frequency_FFT(Image &image);
         bool Menu_Frequency_fftw_fft(Image &image);
         bool Menu_Frequency_fftw_fft_v2(Image &image);
-        bool Menu_Frequency_FFT(Image &image);
-        bool Menu_Frequency_DFT(Image &image);
+        //bool Menu_Frequency_FFT(Image &image);
+        //bool Menu_Frequency_DFT(Image &image);
         bool Menu_Histogram_Stretch(Image &image);
+        bool Menu_Frequency_Ideal_Low_Pass(Image &image);
 };
 
 #endif // MAINWINDOW_H

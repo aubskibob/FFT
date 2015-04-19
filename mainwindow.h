c516fba49fc830633551f0c4cf7741d5be20e04b
mattrichard <matthew.w.richard@gmail.com>
2015-04-19 17:49:52 -0600

    Added ideal high pass filter.

--- mainwindow.h
+++ mainwindow.h
@@ -6,19 +6,19 @@
 
 class MainWindow : public QObject
 {
-    Q_OBJECT;
-
-    public:
-        void fft(fftw_complex* in, fftw_complex* out, int nrows, int ncols, int sign);
+    Q_OBJECT
 
     public slots:
         //bool Menu_Frequency_FFT(Image &image);
         bool Menu_Frequency_fftw_fft(Image &image);
-        bool Menu_Frequency_fftw_fft_v2(Image &image);
         //bool Menu_Frequency_FFT(Image &image);
         //bool Menu_Frequency_DFT(Image &image);
         bool Menu_Histogram_Stretch(Image &image);
         bool Menu_Frequency_Ideal_Low_Pass(Image &image);
+        bool Menu_Frequency_Ideal_High_Pass(Image &image);
+
+    private:
+        void fft(fftw_complex* in, fftw_complex* out, int nrows, int ncols, int sign);
 };
 
 #endif // MAINWINDOW_H
